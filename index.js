6919d3c60aca50fead8aac17c2ef78b773590ca7
Raul Escobedo <rjescobedo@gmail.com>
2025-01-13 08:06:10 -0600

    edits

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Define the function to display astronauts
 
 // Function to display astronauts data
+// Hint: Create a function called displayAstronauts(data)
 function displayAstronauts(data) {
+    // Hint: Select the <ul> element with the id "astronaut-list"
     const astronautList = document.getElementById('astronaut-list');
 
+    // Hint: Loop through the data.people array and create <li> elements for each astronaut's name
     data.people.forEach((person) => {
+        // Hint: Append the <li> elements to the <ul> element
         const listItem = document.createElement('li');
         listItem.textContent = person.name;
         astronautList.append(listItem);
         
     });
 }
-// Hint: Create a function called displayAstronauts(data)
-// Hint: Select the <ul> element with the id "astronaut-list"
-// Hint: Loop through the data.people array and create <li> elements for each astronaut's name
-// Hint: Append the <li> elements to the <ul> element
+
 
 // Fetch data from the external API
-
 // Hint: Use fetch() to make an HTTP request to "http://api.open-notify.org/astros.json"
 fetch('http://api.open-notify.org/astros.json')
 // Hint: Use .then() to process the response and convert it to JSON
